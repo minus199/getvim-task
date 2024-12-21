6cd52b6c15cc2528c7fc141d7e20cb4153cfdd7c
Asaf Blum <minus199@gmail.com>
2024-12-21 12:03:04 +0200

    service contracts + infra

---
+++ src/contracts.ts
@@ -0,0 +1,34 @@
+export type NotificationChannel = 'email' | 'sms';
+
+export type NotificationChannelPreferences = {
+  [key in NotificationChannel]?: string;
+};
+export type NotificationChannelToggle = {
+  [key in NotificationChannel]?: boolean;
+};
+
+export interface UserNotificationPreferences {
+  userId: number;
+  email: string;
+  telephone: string;
+  preferences: NotificationChannelToggle;
+}
+
+export interface NewNotificationDTO {
+  userId: number;
+  message: string;
+}
+
+export interface SendNotificationResponse {
+  status: 'sent';
+  channel: NotificationChannel;
+  to: string;
+  message: string;
+}
+
+export interface EditUserPreferencesDTO {
+  email: string;
+  preferences: NotificationChannelToggle;
+}
+
+export type NewUserPreferencesDTO = Omit<UserNotificationPreferences, 'userId'>;
