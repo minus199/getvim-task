51de1ebd3468d16797780895688ec44eed19a897
Asaf Blum <minus199@gmail.com>
2024-12-21 12:04:53 +0200

    app controller

---
+++ src/app.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Post, Put } from '@nestjs/common';
+import { AppService } from './app.service';
+import {
+  EditUserPreferencesDTO,
+  NewNotificationDTO,
+  NewUserPreferencesDTO,
+  UserNotificationPreferences,
+} from './contracts';
+
+@Controller('/notifications')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post('/')
+  async newUserNotificationsPreferences(
+    @Body() body: NewUserPreferencesDTO,
+  ): Promise<UserNotificationPreferences> {
+    return this.appService.newUserNotificationsPreferences(body);
+  }
+
+  @Put('/')
+  async editUserNotificationsPreferences(
+    @Body() { email, preferences }: EditUserPreferencesDTO,
+  ): Promise<boolean> {
+    return await this.appService.editUserNotificationsPreferences(
+      email,
+      preferences,
+    );
+  }
+
+  @Post('/')
+  async newNotifications(
+    @Body() { userId, message }: NewNotificationDTO,
+  ): Promise<boolean> {
+    await this.appService.dispatchForUserChannel(userId, message);
+    return true;
+  }
+}
