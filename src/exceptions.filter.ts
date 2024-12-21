df783cff4e0718701ed0c7415de489e78ea8c99c
Asaf Blum <minus199@gmail.com>
2024-12-21 13:11:08 +0200

    exceptions and exceptions.filter.ts

---
+++ src/exceptions.filter.ts
@@ -0,0 +1,26 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { BaseError } from './exceptions';
+
+@Catch(BaseError)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = HttpStatus.FORBIDDEN;
+
+    response.status(status).json({
+      message: exception.message,
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
